058256d051194d907e1d8a9199873c64a66423cf
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-27 20:39:30 +0300

    DB connectio #17

--- server/app.js
+++ server/app.js
@@ -1,9 +1,6 @@
 const { join } = require('path');
-require('env2')('./config.env');
 const express = require('express');
 const compression = require('compression');
-const cookieParse = require('cookie-parser');
-const router = require('./routes');
 
 const app = express();
 
@@ -13,9 +10,6 @@ app.use(compression());
 app.disabled('x-powered-by');
 app.set('port', process.env.PORT || 5000);
 
-app.use(express.static(join(__dirname, '..', 'public')));
-
-app.use(cookieParse());
-app.use(router);
+app.use(express.static(join(__dirname, '../public')));
 
 module.exports = app;
