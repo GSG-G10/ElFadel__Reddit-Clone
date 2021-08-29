fd51523799a8bd3cd5a9df5cfa67e1ed44a77bb5
ElhamFadel <elham2000Fadel@gmail.com>
2021-08-29 17:14:41 +0300

    controller delete #26

--- server/controllers/post/deletePost.js
+++ server/controllers/post/deletePost.js
@@ -4,6 +4,6 @@ module.exports = (req, res, next) => {
   const { id: userId } = req.dataUser;
   const { id: postId } = req.params;
   deletePost(userId, postId).then(() => {
-    console.log('Post deleted');
-  });
+    res.redirect('back');
+  }).catch((err) => next(err));
 };
